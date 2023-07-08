6ceaba4aec1b4c9ab7e9eee81dc4a695519528f5
Sleepy-sunflower <126541226+Sleepy-sunflower@users.noreply.github.com>
2023-07-08 18:58:20 +0800

    Add files via upload

---
+++ astro/button.h
@@ -0,0 +1,32 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <QWidget>
+#include<QPushButton>
+#include<QString>
+#include <QSoundEffect>
+
+class button : public QPushButton
+{
+    Q_OBJECT
+private:
+    QPixmap img1;
+    QPixmap img2;
+public:
+    //explicit button(QWidget *parent = nullptr);
+    button(QString str,QString str2);
+    ~button();
+
+    void enterEvent(QEnterEvent *event);
+    void leaveEvent(QEvent *event);
+    void mouseReleaseEvent(QMouseEvent *e);
+    //void init();
+    int diffh;
+    int diffw;
+    QSoundEffect click;
+
+signals:
+
+};
+
+#endif // BUTTON_H
